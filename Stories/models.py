5132c153460c43d7f46e08f03fa98e83d2475119
Kuleb1n <v_vpyt@mail.ru>
2022-11-05 20:52:39 +0300

    creating a model

--- Stories/models.py
+++ Stories/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Story(models.Model):
+    title = models.CharField(max_length=64, verbose_name='Heading')
+    content = models.TextField(verbose_name='content')
+    published = models.DateTimeField(auto_now_add=True, verbose_name='Date of publication')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Category')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Story'
+        verbose_name_plural = 'Stories'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Category')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
