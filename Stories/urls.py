01bc7e884f63d6ffd9ecda09d37b40236c363bbe
Kuleb1n <v_vpyt@mail.ru>
2022-11-06 17:02:58 +0300

    adding routes

--- Stories/urls.py
+++ Stories/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('category/<int:category_id>/', by_category, name='by_category'),
+    path('story/<int:story_id>/', read_more, name='read_more'),
 ]
