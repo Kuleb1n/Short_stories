9d7beaec5a6bc66aba906a9633eecfb3185e2eb6
Kuleb1n <v_vpyt@mail.ru>
2022-11-06 17:03:43 +0300

    adding functions to routes

--- Stories/views.py
+++ Stories/views.py
@@ -8,3 +8,25 @@ def index(request):
         'categories': Category.objects.all(),
     }
     return render(request, 'Stories/index.html', content)
+
+
+def by_category(request, category_id):
+    stories = Story.objects.filter(category=category_id)
+    categories = Category.objects.all()
+    current_category = Category.objects.get(pk=category_id)
+    content = {
+        'stories': stories,
+        'categories': categories,
+        'current_category': current_category,
+    }
+    return render(request, 'Stories/category.html', content)
+
+
+def read_more(request, story_id):
+    categories = Category.objects.all()
+    current_story = Story.objects.get(pk=story_id)
+    content = {
+        'categories': categories,
+        'current_story': current_story,
+    }
+    return render(request, 'Stories/story.html', content)
