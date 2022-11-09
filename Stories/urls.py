b707b055ae6823f1b77e30904c2e634633c98c9d
Kuleb1n <v_vpyt@mail.ru>
2022-11-09 20:22:45 +0300

    adding a route 'add_story/'

--- Stories/urls.py
+++ Stories/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('category/<int:category_id>/', by_category, name='by_category'),
     path('story/<int:story_id>/', read_more, name='read_more'),
+    path('add_story/', add_story, name='add_story'),
 ]
